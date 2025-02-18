254346a3d6e69033b233e3e658c8c2ad04cd5f3b
abeebdon <abeebdon@gmail.com>
2025-02-18 06:45:09 +0100

    Adding modification to login and signup screen, started working on otp verification page

---
+++ src/app/(auth)/otp/page.tsx
@@ -0,0 +1,83 @@
+'use client'
+import { Button } from '@/components/basics'
+import { LogoIcon } from '@/components/icons'
+import { useState } from 'react'
+import OTPInput from 'react-otp-input'
+
+const OtpVerification = () => {
+  const [otp, setOtp] = useState<string>('')
+  const [otpError, setOtpError] = useState<boolean>(false)
+  const containerStyling = {
+    display: 'flex',
+    gap: '20px',
+    justifyContent: 'center ',
+    alignItems: 'center',
+  }
+  const inputStyling = {
+    width: '70px',
+    height: '70px',
+    fontSize: '20px',
+  }
+  const handleOtpVerification = (e: any) => {
+    e.preventDefault()
+    if (otp.length < 4) {
+      setOtpError(true)
+      return
+    }
+    console.log(otp)
+  }
+  return (
+    <section>
+      <div className="flex justify-center flex-col items-center gap-y-5">
+        <LogoIcon className="fill-akauntme size-10" type="alone" />
+        <div className="text-center">
+          <h1 className="text-2xl md:text-3xl font-bold text-akauntme">
+            Sign In to Akauntme
+          </h1>
+          <p className="mt-2 md:mt-5 font-base italic text-indigo-500">
+            Your Smart Accounting Padi...{' '}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <h3>Verify</h3>
+        <p>Please verify the code sent to </p>
+      </div>
+      <form onSubmit={handleOtpVerification} onClick={() => setOtpError(false)}>
+        <OTPInput
+          value={otp}
+          onChange={setOtp}
+          shouldAutoFocus
+          numInputs={4}
+          containerStyle={containerStyling}
+          inputStyle={inputStyling}
+          renderInput={(props) => (
+            <input
+              {...props}
+              inputMode="numeric"
+              pattern="[0-9]*"
+              onKeyDown={(e) => {
+                if (!/^\d$/.test(e.key) && e.key !== 'Backspace') {
+                  e.preventDefault()
+                }
+              }}
+            />
+          )}
+        />
+        {otpError && <p>Please enter a valid OTP</p>}
+        <Button
+          className=" text-blue-500 cursor-pointer"
+          type="submit"
+          title="Verify OTP"
+        />
+      </form>
+      <div>
+        <p>00:30</p>
+      </div>
+      <Button className="underline text-blue-500" title="Resend code" />
+    </section>
+  )
+}
+
+export default OtpVerification
--- src/components/basics/AuthCard.tsx
+++ src/components/basics/AuthCard.tsx
@@ -1,15 +1,12 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react'
 
 interface AuthCardProps {
-    LeftPane: ReactNode;
-    RightPane: ReactNode;
+  LeftPane: ReactNode
+  RightPane: ReactNode
 }
 
 const AuthCard: React.FC<AuthCardProps> = ({ LeftPane, RightPane }) => (
-    <div className="min-h-screen flex flex-col lg:flex-row py-6 sm:py-10 mx-2  overflow-hidden">
-        <div className="w-full lg:w-6/12">{LeftPane}</div>
-        <div className="hidden lg:block w-full lg:w-6/12">{RightPane}</div>
-    </div>
-);
+  <div className="min-h-screen flex flex-col lg:flex-row py-6 sm:py-10 mx-2  overflow-hidden"></div>
+)
 
-export default AuthCard;
+export default AuthCard
